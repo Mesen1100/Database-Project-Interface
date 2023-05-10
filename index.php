2ebd8fd96b0f93a32f1c9b94bd178d2d4429bf38
Mustafa Esen <mesen1100@gmail.com>
2023-05-10 18:21:18 +0300

    Add Basic Html code for index

--- index.php
+++ index.php
@@ -1,12 +1,64 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
     <title>Bank Database Interface</title>
+    <script>
+        setTimeout(() => {
+            document.location.reload();
+        }, 5000);
+    </script>
 </head>
+
 <body>
-    
+    <div class="row text-center">
+        <div class="col-md-4">
+            <a href="account/account.php" target="_blank">
+            <img src="assets/img/account.png" alt="Account">
+            <h1>ACCOUNT</h1></a>
+        </div>
+        <div class="col-md-4">
+            <img src="assets/img/accounttype.png" alt="Account Type">
+            <h1>ACCOUNT TYPE</h1>
+        </div>
+        <div class="col-md-4">
+            <img src="assets/img/branch.png" alt="Branch">
+            <h1>BRANCH</h1>
+        </div>
+        <div class="col-md-4">
+            <img src="assets/img/customer.png" alt="Customer">
+            <h1>CUSTOMER</h1>
+        </div>
+        <div class="col-md-4">
+             <img src="assets/img/loan.png" alt="Loan">
+            <h1>LOAN</h1>
+        </div>
+        <div class="col-md-4">
+            <img src="assets/img/loantype.png" alt="Loan Type">
+            <h1>LOANTYPE</h1>
+        </div>
+        <div class="col-md-4">
+            <img src="assets/img/staff.png" alt="Staff">
+            <h1>STAFF</h1>
+        </div>
+        <div class="col-md-4">
+            <img src="assets/img/transactions.png" alt="Transaction Type">
+            <h1>TRANSACTIONS</h1>
+        </div>
+        <div class="col-md-4">
+            <img src="assets/img/transactionstype.png" alt="Transactions Type">
+            <h1>TRASACTIONS TYPE</h1>
+        </div>
+
+
+
+    </div>
+
 </body>
+
 </html>
