6086332e262205c8efc44262fcc8aa504f3ed227
Mustafa Esen <mesen1100@gmail.com>
2023-05-10 18:38:52 +0300

    Add necessary web pages and edit this web pages

---
+++ pages/branch/branch.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <title>Branch</title>
+    <link rel="shortcut icon" href="../../assets/img/branch.png" type="image/x-icon">
+    <script>
+        setTimeout(() => {
+            document.location.reload();
+        }, 5000);
+    </script>
+</head>
+
+<body>
+    <div class="text-center">
+        <h1>BRANCH</h1>
+    </div>
+</body>
+
+</html>
