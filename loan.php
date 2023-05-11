ed15609c693304a68d568753473ecb3a3e000cc5
Mustafa Esen <mesen1100@gmail.com>
2023-05-11 19:54:38 +0300

    Connect Database fo Loan Table

--- loan.php
+++ loan.php
@@ -17,9 +17,55 @@
 </head>
 
 <body>
-<?php include "includes/navbar.php";?>
+    <?php include "includes/navbar.php"; ?>
     <div class="text-center">
         <h1>LOAN</h1>
+    </div>
+    <?php
+    $loansor = $db->prepare("select * from loan");
+    $loansor->execute();
+    $loangetir = $loansor->fetchAll(PDO::FETCH_ASSOC); ?>
+
+    <div class="card-body text-center">
+        <table id="example2" class="table table-bordered table-hover">
+            <thead>
+                <tr>
+                    <th>Loan ID</th>
+                    <th>Loan Type ID</th>
+                    <th>Loan Interest</th>
+                    <th>Loan Amount</th>
+                    <th>Date of Create</th>
+                    <th>Date of Delete</th>
+                    <th>Date of Issued</th>
+                    <th>Collateral</th>
+                    <th>Dept Income Ratio</th>
+                    <th>Loan Term</th>
+                    <th>Account Number</th>
+                </tr>
+            </thead>
+            <?php foreach ($loangetir as $row) {
+
+            ?>
+                <tr>
+                    <td><?php echo $row['loanID']; ?></td>
+                    <td><?php echo $row['loanTypeID']; ?></td>
+                    <td><?php echo $row['loanInterest']; ?></td>
+                    <td><?php echo $row['loanAmount']; ?></td>
+                    <td><?php echo $row['dateofCreate']; ?></td>
+                    <td><?php echo $row['dateOfDelete']; ?></td>
+                    <td><?php echo $row['dateOfIssued']; ?></td>
+                    <td><?php echo $row['collateral']; ?></td>
+                    <td><?php echo $row['deptIncomeRatio']; ?></td>
+                    <td><?php echo $row['loanTerm']; ?></td>
+                    <td><?php echo $row['accountNumber']; ?></td>
+
+
+                </tr>
+            <?php } ?>
+        </table>
+    </div>
+
+
     </div>
 </body>
 
