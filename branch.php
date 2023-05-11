e5ad320cc1a0511c624cb6030b417f3db6a0525b
Mustafa Esen <mesen1100@gmail.com>
2023-05-11 19:41:17 +0300

    Connect Database for Branch table

--- branch.php
+++ branch.php
@@ -21,6 +21,47 @@
     <div class="text-center">
         <h1>BRANCH</h1>
     </div>
+    <?php
+    $branchsor = $db->prepare("select * from branch");
+    $branchsor->execute();
+    $branchgetir = $branchsor->fetchAll(PDO::FETCH_ASSOC); ?>
+
+    <div class="card-body text-center">
+        <table id="example2" class="table table-bordered table-hover">
+            <thead>
+                <tr>
+                    <th>Branch ID</th>
+                    <th>Branch Name</th>
+                    <th>Branch Address</th>
+                    <th>Branch Schedule</th>
+                    <th>Branch Tel Number</th>
+                    <th>Branch Number of Employees</th>
+                    <th>Branch Number of ATMs</th>
+                    <th>Branch Manager ID</th>
+                    
+                </tr>
+            </thead>
+            <?php foreach ($branchgetir as $row) {
+
+            ?>
+                <tr>
+                    <td><?php echo $row['branchID']; ?></td>
+                    <td><?php echo $row['branchName']; ?></td>
+                    <td><?php echo $row['branchAddress']; ?></td>
+                    <td><?php echo $row['branchSchedule']; ?></td>
+                    <td><?php echo $row['branchTelNumber']; ?></td>
+                    <td><?php echo $row['branchNumberofEmployees']; ?></td>
+                    <td><?php echo $row['branchNumberofATMs']; ?></td>
+                    <td><?php echo $row['branchManagerID']; ?></td>
+
+                    
+                </tr>
+            <?php } ?>
+        </table>
+    </div>
+
+
+    </div>
 </body>
 
 </html>
