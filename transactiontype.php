6dfe8e6d0d84608a44e4d7b616a5162e34503a55
Mustafa Esen <mesen1100@gmail.com>
2023-05-11 20:07:58 +0300

    Connect Database for Transaction Type Table

--- transactiontype.php
+++ transactiontype.php
@@ -21,6 +21,34 @@
     <div class="text-center">
         <h1>TRANSACTIONS TYPE</h1>
     </div>
+    <?php
+    $transactiontypesor = $db->prepare("select * from transactiontype");
+    $transactiontypesor->execute();
+    $transactiontypegetir = $transactiontypesor->fetchAll(PDO::FETCH_ASSOC); ?>
+
+    <div class="card-body text-center">
+        <table id="example2" class="table table-bordered table-hover">
+            <thead>
+                <tr>
+                    <th>Transaction Type ID</th>
+                    <th>Transaction Type</th>
+                    
+                </tr>
+            </thead>
+            <?php foreach ($transactiontypegetir as $row) {
+
+            ?>
+                <tr>
+                    <td><?php echo $row['transactionTypeID']; ?></td>
+                    <td><?php echo $row['transactionType']; ?></td>
+                    
+                </tr>
+            <?php } ?>
+        </table>
+    </div>
+
+
+    </div>
 </body>
 
 </html>
