76b4062be68ff9cd2e81fa1594bcd7c853565dc2
Mustafa Esen <mesen1100@gmail.com>
2023-05-11 19:46:02 +0300

    Connect Database for Customer Table

--- customer.php
+++ customer.php
@@ -21,6 +21,49 @@
     <div class="text-center">
         <h1>CUSTOMER</h1>
     </div>
+    <?php
+    $customersor = $db->prepare("select * from customer");
+    $customersor->execute();
+    $customergetir = $customersor->fetchAll(PDO::FETCH_ASSOC); ?>
+
+    <div class="card-body text-center">
+        <table id="example2" class="table table-bordered table-hover">
+            <thead>
+                <tr>
+                    <th>Customer ID</th>
+                    <th>Customer Name</th>
+                    <th>Customer Last Name</th>
+                    <th>Date of Birth</th>
+                    <th>Customer City</th>
+                    <th>Customer Tel No</th>
+                    <th>Customer Email</th>
+                    <th>Customer Address</th>
+                    <th>Branch ID</th>
+                    
+                </tr>
+            </thead>
+            <?php foreach ($customergetir as $row) {
+
+            ?>
+                <tr>
+                    <td><?php echo $row['customerID']; ?></td>
+                    <td><?php echo $row['customerName']; ?></td>
+                    <td><?php echo $row['customerLastName']; ?></td>
+                    <td><?php echo $row['dateofBirth']; ?></td>
+                    <td><?php echo $row['customerCity']; ?></td>
+                    <td><?php echo $row['customerTelNo']; ?></td>
+                    <td><?php echo $row['customerEmail']; ?></td>
+                    <td><?php echo $row['customerAddress']; ?></td>
+                    <td><?php echo $row['branchID']; ?></td>
+
+                    
+                </tr>
+            <?php } ?>
+        </table>
+    </div>
+
+
+    </div>
 </body>
 
 </html>
