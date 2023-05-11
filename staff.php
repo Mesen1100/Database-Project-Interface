84299c49228ef211ff4438b84177791fa44a68c3
Mustafa Esen <mesen1100@gmail.com>
2023-05-11 20:01:13 +0300

    Connect Database for Staff Table

--- staff.php
+++ staff.php
@@ -21,6 +21,52 @@
     <div class="text-center">
         <h1>STAFF</h1>
     </div>
+    <?php
+    $staffsor = $db->prepare("select * from staff");
+    $staffsor->execute();
+    $staffgetir = $staffsor->fetchAll(PDO::FETCH_ASSOC); ?>
+
+    <div class="card-body text-center">
+        <table id="example2" class="table table-bordered table-hover">
+            <thead>
+                <tr>
+                    <th>Staff ID</th>
+                    <th>Staff Name</th>
+                    <th>Staff Last Name</th>
+                    <th>Staff Salary</th>
+                    <th>Staff Tel No</th>
+                    <th>Staff Email</th>
+                    <th>Staff Date of Birth</th>
+                    <th>Staff Schdule</th>
+                    <th>Staff Start of Work Date</th>
+                    <th>Staff Job Title</th>
+                    <th>Branch ID</th>
+                </tr>
+            </thead>
+            <?php foreach ($staffgetir as $row) {
+
+            ?>
+                <tr>
+                    <td><?php echo $row['staffID']; ?></td>
+                    <td><?php echo $row['staffName']; ?></td>
+                    <td><?php echo $row['staffLastName']; ?></td>
+                    <td><?php echo $row['staffSalary']; ?></td>
+                    <td><?php echo $row['staffTelNo']; ?></td>
+                    <td><?php echo $row['staffEmail']; ?></td>
+                    <td><?php echo $row['staffDob']; ?></td>
+                    <td><?php echo $row['staffSchdule']; ?></td>
+                    <td><?php echo $row['staffStartofWorkDate']; ?></td>
+                    <td><?php echo $row['staffJobTitle']; ?></td>
+                    <td><?php echo $row['branchID']; ?></td>
+
+                    
+                </tr>
+            <?php } ?>
+        </table>
+    </div>
+
+
+    </div>
 </body>
 
 </html>
