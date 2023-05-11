bc5e6ffa8fda2487168e6e53916ea9ae4c98aedd
Mustafa Esen <mesen1100@gmail.com>
2023-05-11 20:06:40 +0300

    Connect Database for Transaction Table

--- transaction.php
+++ transaction.php
@@ -21,6 +21,49 @@
     <div class="text-center">
         <h1>TRANSACTIONS</h1>
     </div>
+    <?php
+    $transactionssor = $db->prepare("select * from transactions");
+    $transactionssor->execute();
+    $transactionsgetir = $transactionssor->fetchAll(PDO::FETCH_ASSOC); ?>
+
+    <div class="card-body text-center">
+        <table id="example2" class="table table-bordered table-hover">
+            <thead>
+                <tr>
+                    <th>Transaction ID</th>
+                    <th>Sender Account Number</th>
+                    <th>Receive Account Number</th>
+                    <th>Amount</th>
+                    <th>Date of Create</th>
+                    <th>Date of Done</th>
+                    <th>Transaction Type ID</th>
+                    <th>Description</th>
+                    <th>Transaction State</th>
+                    
+                </tr>
+            </thead>
+            <?php foreach ($transactionsgetir as $row) {
+
+            ?>
+                <tr>
+                    <td><?php echo $row['transactionID']; ?></td>
+                    <td><?php echo $row['senderAccountNumber']; ?></td>
+                    <td><?php echo $row['receiveAccountNumber']; ?></td>
+                    <td><?php echo $row['amount']; ?></td>
+                    <td><?php echo $row['dateOfCreate']; ?></td>
+                    <td><?php echo $row['dateOfDone']; ?></td>
+                    <td><?php echo $row['transactionTypeID']; ?></td>
+                    <td><?php echo $row['description']; ?></td>
+                    <td><?php echo $row['transactionState']; ?></td>
+
+                    
+                </tr>
+            <?php } ?>
+        </table>
+    </div>
+
+
+    </div>
 </body>
 
 </html>
